c5f0d666d58c954565548f5bba77ae0f62767dbf
Relixxxy <70975131+Relixxxy@users.noreply.github.com>
2022-09-17 16:38:12 +0000

    Deploying to gh-pages from @ Relixxxy/WebStudio_3.0@5edc8d0a1c2f9af28cfa4bb32a78378e89774fe3 🚀

---
+++ portfolio.6f698e50.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(" [data-menu-open] "),closeMenuBtn:document.querySelector(" [data-menu-close] "),menu:document.querySelector(" [data-menu] ")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=portfolio.6f698e50.js.map
